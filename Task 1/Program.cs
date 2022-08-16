4402b7024b5a1c5ec1beb5848664b74ad8867a65
Savely <Savka9765@yandex.ru>
2022-08-16 14:13:37 +0300

    Task 1. Решение без цикла

---
+++ Task 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число A: ");
+int number_A= Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите степень числа A: ");
+int degree_A= Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(number_A, degree_A);
+
+Console.Write($"Число {number_A} в степени {degree_A} = {result}");
