88000ad6cd6bf752afbe62e9db89602db8be893b
Савелий <Savka9765@yandex.ru>
2022-08-16 20:11:39 +0300

    Task 1. Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,9 +1,19 @@
-﻿Console.Write("Введите число A: ");
-int number_A= Convert.ToInt32(Console.ReadLine()); 
+﻿/*цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int number_A = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите степень числа A: ");
-int degree_A= Convert.ToInt32(Console.ReadLine());
+int degree_A = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Pow(number_A, degree_A);
+double result = 0;
+
+for (int count = 0; count <= degree_A; count++)
+{
+    result = Math.Pow(number_A, count);
+}
 
 Console.Write($"Число {number_A} в степени {degree_A} = {result}");
