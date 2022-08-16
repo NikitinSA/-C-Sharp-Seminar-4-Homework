5df8b63e5d6a9f4f1b51af4ba720df67f4480436
Савелий <Savka9765@yandex.ru>
2022-08-16 22:20:27 +0300

    Необходимо доделать

---
+++ Task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigitInANumber(int user_number)
+{
+    int result =+ rem; 
+    while (user_number > 0)
+    {
+        Console.WriteLine(user_number);
+        int rem = user_number % 10;
+        Console.WriteLine(rem);
+        int res1 = rem;
+        int sub = user_number - rem;
+        Console.WriteLine(sub);
+        Console.WriteLine(result);
+        if (sub % 10 == 0)
+        {
+            user_number = user_number / 10;
+        }
+    }
+    return result;
+}
+
+int result = SumOfDigitInANumber(user_number);
+Console.Write($"Сумма цифр в числе {user_number} = {result}");
+
+
+
