74afda01db8e25913e0ed4179b2bab7283731b94
Savely <Savka9765@yandex.ru>
2022-08-17 10:50:22 +0300

    Task 3. Программа, которая задаёт массив из 8 элементов и выводит их на экран.

---
+++ Task 3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.*/
+
+Console.Write("Введите число из которого должен состоять массив: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[N];
+
+void Massive(int[] array)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(100);
+        a[i] = value;
+        Console.Write($"{a[i]} ");
+    }
+}
+
+Massive(a);
