af2882a04dc8332f1fff8b5587a283d43fbe628e
Savely <Savka9765@yandex.ru>
2022-08-17 09:11:52 +0300

    Task 2. Программа, которая принимает на вход число и выдаёт сумму цифр в числе.

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -8,26 +8,14 @@ int user_number = Convert.ToInt32(Console.ReadLine());
 
 int SumOfDigitInANumber(int user_number)
 {
-    int result =+ rem; 
+    int result = 0;
     while (user_number > 0)
     {
-        Console.WriteLine(user_number);
-        int rem = user_number % 10;
-        Console.WriteLine(rem);
-        int res1 = rem;
-        int sub = user_number - rem;
-        Console.WriteLine(sub);
-        Console.WriteLine(result);
-        if (sub % 10 == 0)
-        {
-            user_number = user_number / 10;
-        }
+        result = result + user_number % 10;
+        user_number = user_number / 10;
     }
     return result;
 }
 
 int result = SumOfDigitInANumber(user_number);
 Console.Write($"Сумма цифр в числе {user_number} = {result}");
-
-
-
